346f31c9c15cc4b53e31969015721b485b64c6a4
Isaac Perez <Isaac.Perez.1@outlook.com>
2025-01-16 21:35:35 -0800

    created test case and logic for starting player as x. Also created method to swap player.

---
+++ TicTacToe/TicTacToe/PlayerClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class PlayerClass
+    {
+        public char currentPlayer;
+        public  PlayerClass()
+        {
+            currentPlayer = 'X';
+        }
+
+        public void SwapPlayer()
+        {
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+        }
+    }
+}
---
+++ TicTacToe/TicTacToeTest/PlayerClassTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToe;
+
+namespace TicTacToeTest
+{
+    [TestClass]
+    public class PlayerClassTest
+    {
+        [TestMethod]
+        public void GameStartsWithPlayerX()
+        {
+            // Arrange
+            var player = new PlayerClass(); // PlayerClass initializes currentPlayer as 'X'
+
+            // Act
+            var startingPlayer = player.currentPlayer;
+
+            // Assert
+            Assert.AreEqual('X', startingPlayer, "The game should start with Player X.");
+        }
+    }
+}
